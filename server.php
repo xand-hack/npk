a22cfac077ca65a5edc882e60e3b4f0e61eb1585
xand-hack <128232845+xand-hack@users.noreply.github.com>
2024-04-08 15:40:01 +0300

    Add files via upload

---
+++ server.php
@@ -0,0 +1,48 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+
+if (isset($_POST['mealCount']) && isset($_POST['choseMenu'])) {
+    $mealCount = $_POST['mealCount'];
+    $choseMenu = $_POST['choseMenu'];
+
+    require_once "db.php";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT name, composition, protein, fat, carbohydrates, meal_type FROM meals WHERE type = '$choseMenu'";
+
+    if ($mealCount == 3) {
+        $sql .= " AND (meal_type = 'breakfast' OR meal_type = 'lunch' OR meal_type = 'dinner')";
+    } elseif ($mealCount == 4) {
+        $sql .= " AND (meal_type = 'breakfast' OR meal_type = 'lunch' OR meal_type = 'dinner' OR meal_type = 'snack')";
+    }
+
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $data = array();
+
+        while ($row = $result->fetch_assoc()) {
+            $data[] = array(
+                'name' => $row['name'],
+                'composition' => $row['composition'],
+                'protein' => $row['protein'],
+                'fat' => $row['fat'],
+                'carbohydrates' => $row['carbohydrates'],
+                'meal_type' => $row['meal_type']
+            );
+        }
+
+        echo json_encode($data);
+    } else {
+        echo json_encode(array('message' => 'Нет подходящих вариантов'));
+    }
+
+
+    $conn->close();
+} else {
+    echo json_encode(array('message' => 'Переменные получаемые не определены'));
+}
